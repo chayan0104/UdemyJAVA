dc9b1571453b187657fc8584b8a83bd6f88b331f
mecha <bhcdsc@gmail.com>
2023-02-02 00:21:15 +0530

    Java

---
+++ src/com/methods/java/MethodEx.java
@@ -0,0 +1,22 @@
+package com.methods.java;
+
+public class MethodEx {
+    public static void main(String[] args) {
+        calculateScore(10000,8,200,true);
+    }
+    //another methods
+    public static void calculateScore(int score,int levelCompleted,int bonus,boolean gameOver){
+        /*int score = 10000;
+        int levelCompleted =8;
+        int bonus =200;
+        boolean gameOver = true;
+*/
+        int finalScore = score;
+
+        if(gameOver){
+            finalScore += (bonus*levelCompleted);
+            finalScore +=1000;
+            System.out.println("final Score : " + finalScore);
+        }
+    }
+}
