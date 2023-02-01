148e64cc92d4b473caa1b832d9e11cc16df4657b
mecha <bhcdsc@gmail.com>
2023-02-01 20:44:55 +0530

    Java

---
+++ src/com/basics/java/FirstClass.java
@@ -0,0 +1,7 @@
+package com.basics.java;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.print("Hello, Tim");
+    }
+}
---
+++ src/com/basics/java/SecondClass.java
@@ -0,0 +1,7 @@
+package com.basics.java;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.print("Hello, Tim");
+    }
+}
