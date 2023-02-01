020438a651353e33824b28efb1b12d757fa8a8e2
mecha <bhcdsc@gmail.com>
2023-02-01 21:33:39 +0530

    Java

--- src/com/basics/java/Prac1.java
+++ src/com/basics/java/Prac1.java
@@ -9,9 +9,10 @@ public class Prac1 {
 
         //reminder
         Double rem = mul%40.00d;
+        System.out.println(rem + " is the reminder");
         //it will check the reminder is 0 or not.
         boolean isNotReminder =(rem == 0)? true :false;
-        System.out.println("the ans is " + isNotReminder);
+        System.out.println("the ans is " + isNotReminder + " as we checked for equally 0 or not");
 
         //if else statement
         if(!isNotReminder){
