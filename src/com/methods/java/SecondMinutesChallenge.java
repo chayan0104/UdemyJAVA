d3bd3570467722008c611335b6b19bd05ea83339
mecha <bhcdsc@gmail.com>
2023-02-05 00:45:58 +0530

    Java

--- src/com/methods/java/SecondMinutesChallenge.java
+++ src/com/methods/java/SecondMinutesChallenge.java
@@ -1,11 +1,8 @@
 package com.methods.java;
-
 public class SecondMinutesChallenge {
     public static void main(String[] args) {
         System.out.println(getDuraationString(3945));
         System.out.println(getDuraationString(45,65));
-
-
     }
     public  static String getDuraationString(int seconds){
         int minutes = seconds/60;
@@ -15,8 +12,6 @@ public class SecondMinutesChallenge {
         int hours = minutes/60;
         int remainingminutes = minutes%60;
         int remainingSeconds = seconds%60;
-
         return hours +"h " + remainingminutes+ "m " + remainingSeconds +"s ";
-
     }
 }
